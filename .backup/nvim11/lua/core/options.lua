1e977638679502c669b841c047ff670ab3fb6051
zxhtom <870775401@qq.com>
2025-06-17 15:15:06 +0800

    modify structure && single file mapping plugin to provide back and restore

---
+++ .backup/nvim11/lua/core/options.lua
@@ -0,0 +1,4 @@
+-- 禁用 netrw（使用 Telescope 的文件浏览器替代）
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.mapleader = " "
--- .backup/nvim11/lua/options.lua
+++ .backup/nvim11/lua/options.lua
@@ -1,4 +1,1 @@
--- 禁用 netrw（使用 Telescope 的文件浏览器替代）
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.mapleader = " "
+require('core.options')
