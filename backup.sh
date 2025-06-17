f7b6631b7062b20a91c21f0086f0d4af93ad26e0
zxhtom <870775401@qq.com>
2025-06-17 09:24:24 +0800

    remove telescope ignore files rules avoid telescope project list is empty && modify backup+restore scripts support one to anothers

--- backup.sh
+++ backup.sh
@@ -1,4 +1,10 @@
 DIR=$( dirname "$(readlink -f  ${BASH_SOURCE[0]})" )
+if [[ $# -lt 2 ]]
+then
+  echo "arguments invalid , please give me two"
+  exit
+fi
 nvimname=$1
+target=$2
 mkdir -p ${DIR}/.backup/${nvimname}
-cp -rvf ${HOME}/.config/${nvimname}/lua ${DIR}/.backup/${nvimname}
+cp -rvf ${HOME}/.config/${nvimname}/lua ${DIR}/.backup/${target}
--- restore.sh
+++ restore.sh
@@ -1,3 +1,9 @@
 DIR=$( dirname "$(readlink -f  ${BASH_SOURCE[0]})" )
+if [[ $# -lt 2 ]]
+then
+  echo "arguments invalid , please give me two"
+  exit
+fi
 nvimname=$1
-cp -rvf  ${DIR}/.backup/${nvimname} ${HOME}/.config/
+target=$2
+cp -rvf  ${DIR}/.backup/${nvimname}/* ${HOME}/.config/${target}
